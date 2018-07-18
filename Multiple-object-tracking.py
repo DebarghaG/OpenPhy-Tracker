b30654a4ea656f33522bf1b59081aff89b0dba16
DebarghaG <royal.debargha@gmail.com>
2018-07-18 09:57:35 +0530

    Adding edge detection capabilities

--- Multiple-object-tracking.py
+++ Multiple-object-tracking.py
@@ -1,4 +1,6 @@
 #Code in python to first remove the background from the videoself.
+#Then this code does edge analysis on the image with the background subtracted
+
 
 import numpy as np
 import cv2 as cv
@@ -7,15 +9,25 @@ cap = cv.VideoCapture('vtest.avi')
 
 fgbg = cv.bgsegm.createBackgroundSubtractorMOG()
 
+edges = cv.Canny()
+
 while(1):
     ret, frame = cap.read()
     fgmask = fgbg.apply(frame)
     cv.imshow('frame',fgmask)
+    edge_show = edges(fgmask, 100, 200)
 
+    plt.subplot(121),plt.imshow(img,cmap = 'gray')
+    plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+    plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+    plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+
+    plt.show()
 
     k = cv.waitKey(30) & 0xff
     if k == 27:
         break
+
 
 
 cap.release()
--- README.md
+++ README.md
@@ -7,3 +7,12 @@ This repo contains some brief code that I wrote when I was working with Prof Sab
 The goal is to be able to detect and track objects in the frame, to gain insights into the system behaviour.
 
 The original dataset will not be available due to certain IP reasons, however I'll create a new one and attach it to this repo.
+
+## Object_Tracking_MATLAB.m
+
+Ths extracts the objects and implements a kalman filter to do the same. This is pretty high level code in matlab.
+
+## Multiple-object-tracking.py
+
+This is the code in python to first remove the background from the videoself.
+Then this code does edge analysis on the image with the background subtracted.
